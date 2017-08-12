639e9892c2874bf56ca95cd95605cf9d2420c01b
jayden <zelaga@gmail.com>
2017-08-12 15:56:01 +0900

    make objectivec project01

---
+++ project01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  project01
+//
+//  Created by Jayden on 12/08/2017.
+//  Copyright © 2017 jayden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
